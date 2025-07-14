0376b4ca0c63a400466a95849bfebbc197d1f8f2
Nguyen Minh Doan <nguyenminhdoan0303@gmail.com>
2025-07-14 17:40:57 -0400

    change project structure

---
+++ tests/integration/test_db_connection.py
@@ -0,0 +1,18 @@
+import mysql.connector
+import unittest
+import os
+
+class TestMySQLConnection(unittest.TestCase):
+    def test_connection(self):
+        connection = mysql.connector.connect(
+            host=os.getenv("MYSQL_HOST", "127.0.0.1"),
+            user=os.getenv("MYSQL_USER", "root"),
+            password=os.getenv("MYSQL_PASSWORD", ""),
+            database=os.getenv("MYSQL_DATABASE", "test_db")
+        )
+
+        self.assertTrue(connection.is_connected())
+        connection.close()
+
+if __name__ == '__main__':
+    unittest.main()
