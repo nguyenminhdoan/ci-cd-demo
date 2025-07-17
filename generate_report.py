98172d08e300559e71275c694a81ef8bcfa717be
Rica <rica@devz.com>
2025-07-17 11:54:01 -0400

    test trigger github

--- generate_report.py
+++ generate_report.py
@@ -139,7 +139,7 @@ def generate_html_report():
     <div class="container">
         <div class="header">
             <h1>🚀 CI/CD Demo - Test Report</h1>
-            <p>Automated Testing & Deployment Pipeline test 1</p>
+            <p>Automated Testing & Deployment Pipeline test 2</p>
         </div>
 
         <div class="status">
